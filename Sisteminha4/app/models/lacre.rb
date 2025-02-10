be9fd3aa424784de243fe684c57771c3a0866c5c
Dicommunitas <34188076+Dicommunitas@users.noreply.github.com>
2025-02-10 00:52:38 -0300

    Update lacre.rb
    Alterado para permitir numeração de lacres repetidas.

--- Sisteminha4/app/models/lacre.rb
+++ Sisteminha4/app/models/lacre.rb
@@ -1,7 +1,7 @@
 class Lacre < ActiveRecord::Base
   
   validates_numericality_of :numero, :only_integer => true, :allow_blank => true, :greater_than => 0
-  validates :numero, :uniqueness => true, :allow_blank => true
+  #validates :numero, :uniqueness => true, :allow_blank => true
   validates_presence_of :amostra, :finalidade
   
   belongs_to :amostra
